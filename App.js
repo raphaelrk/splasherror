f8ec36b27349c22f3de517272cb69905ceaabad1
Raphael Rouvinov-Kats <raphael.rouvinov@gmail.com>
2020-04-07 12:13:00 -0400

    init

---
+++ App.js
@@ -0,0 +1,51 @@
+import * as React from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+import { AppLoading } from 'expo';
+
+const instructions = Platform.select({
+  ios: `Press Cmd+R to reload,\nCmd+D or shake for dev menu`,
+  android: `Double tap R on your keyboard to reload,\nShake or press menu button for dev menu`,
+});
+
+const loadAssets = () => new Promise((resolve, reject) => resolve());
+
+export default function App() {
+
+  const [isReady, setIsReady] = React.useState(false);
+  if (!isReady) {
+    return (
+      <AppLoading
+        startAsync={loadAssets}
+        onFinish={() => setIsReady(true)}
+        onError={console.warn}
+      />
+    );
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.welcome}>Welcome to React Native!</Text>
+      <Text style={styles.instructions}>To get started, edit App.js</Text>
+      <Text style={styles.instructions}>{instructions}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
